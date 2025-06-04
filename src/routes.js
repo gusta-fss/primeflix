f34f365f85a506714e65475b3e160832778a98dc
Gustavo Farias <gustavolanefs15@gmail.com>
2025-06-04 20:54:43 -0300

    Remove favoritos

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
-import Favoritos from './pages/Favoritos';
 import Erro from './pages/Erro';
 import Header from './components/Header';
 
@@ -13,7 +12,6 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={ <Home/> }/>             
                 <Route path='/filme/:id' element={ <Filme/> }/>
-                <Route path='/favoritos' element={ <Favoritos/> }/>
                 <Route path='*' element={<Erro/>}/>
             </Routes>
         </BrowserRouter>
@@ -21,5 +19,3 @@ function RoutesApp(){
 }
 
 export default RoutesApp;
-
-//<Route path='/breve' element={<Breve/>}/>
