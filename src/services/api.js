9dac83db299d041479793116a4838c30a42c17fa
Gustavo Farias <gustavolanefs15@gmail.com>
2025-06-04 20:54:57 -0300

    Update api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // BASE URL : https://api.themoviedb.org/3/
-// URL: https://api.themoviedb.org/3/movie/550?api_key=1c2a76cbffe335a09939bf552765e179
+// URL: https://api.themoviedb.org/3/movie/550?api_key=8e9f13c0f25f87fed81750254cf42933
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
